0afaeab1185e0d5657cca3ab11410f1cef82241c
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-07 14:46:43 +0000

    Added Sequelize model for Document table with associations

--- Backend/models/Document.js
+++ Backend/models/Document.js
@@ -1,12 +1,23 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Customer = require('./Customer');
 
 const Document = sequelize.define('Document', {
-    type: { type: DataTypes.STRING },  // e.g., Passport, Driver’s License
-    documentNumber: { type: DataTypes.STRING },
+    type: { type: DataTypes.STRING, allowNull: false },
+    documentNumber: { type: DataTypes.STRING, allowNull: false },
     issueDate: { type: DataTypes.DATE },
     expiryDate: { type: DataTypes.DATE },
-    customerId: { type: DataTypes.INTEGER, references: { model: 'Customers', key: 'id' } }
+    customerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Customer,
+            key: 'customerID',
+        },
+    },
 });
+
+Document.belongsTo(Customer, { foreignKey: 'customerId', as: 'customer' });
+Customer.hasMany(Document, { foreignKey: 'customerId', as: 'documents' });
 
 module.exports = Document;
