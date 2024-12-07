0796f6c4dfc641e1863c005ac4e1b90aa9a499ab
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-07 14:47:53 +0000

    Added Sequelize model for furnitureTable table with associations

--- Backend/models/FurnitureRequirement.js
+++ Backend/models/FurnitureRequirement.js
@@ -1,10 +1,21 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Customer = require('./Customer');
 
 const FurnitureRequirement = sequelize.define('FurnitureRequirement', {
-    amount: { type: DataTypes.DECIMAL },
-    description: { type: DataTypes.TEXT },
-    customerId: { type: DataTypes.INTEGER, references: { model: 'Customers', key: 'id' } }
+    amount: { type: DataTypes.DECIMAL, allowNull: false },
+    description: { type: DataTypes.TEXT, allowNull: false },
+    customerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Customer,
+            key: 'customerID',
+        },
+    },
 });
+
+FurnitureRequirement.belongsTo(Customer, { foreignKey: 'customerId', as: 'customer' });
+Customer.hasMany(FurnitureRequirement, { foreignKey: 'customerId', as: 'furnitureRequirements' });
 
 module.exports = FurnitureRequirement;
