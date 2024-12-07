b5fbc632627165a13b0fd7fd027b96f7dc82cde9
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-07 14:48:17 +0000

    Added Sequelize model for Mortgage table with associations

--- Backend/models/MortgageDocument.js
+++ Backend/models/MortgageDocument.js
@@ -1,11 +1,22 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Customer = require('./Customer');
 
 const MortgageDocument = sequelize.define('MortgageDocument', {
-    documentType: { type: DataTypes.STRING },
-    documentNumber: { type: DataTypes.STRING },
+    documentType: { type: DataTypes.STRING, allowNull: false },
+    documentNumber: { type: DataTypes.STRING, allowNull: false },
     issueDate: { type: DataTypes.DATE },
-    customerId: { type: DataTypes.INTEGER, references: { model: 'Customers', key: 'id' } }
+    customerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Customer,
+            key: 'customerID',
+        },
+    },
 });
+
+MortgageDocument.belongsTo(Customer, { foreignKey: 'customerId', as: 'customer' });
+Customer.hasMany(MortgageDocument, { foreignKey: 'customerId', as: 'mortgageDocuments' });
 
 module.exports = MortgageDocument;
