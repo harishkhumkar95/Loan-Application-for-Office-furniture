0cd40cd302525dad1a0e12a97965a18f910a2a10
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-07 17:03:11 +0000

    Added approval status field in customer.js

--- Backend/models/Customer.js
+++ Backend/models/Customer.js
@@ -11,6 +11,7 @@ const Customer = sequelize.define('Customer', {
     name: { type: DataTypes.STRING, allowNull: false },
     email: { type: DataTypes.STRING, allowNull: false },
     phone: { type: DataTypes.STRING },
+    approvalStatus: { type: DataTypes.STRING, defaultValue: 'pending' }
 });
 
 module.exports = Customer;
