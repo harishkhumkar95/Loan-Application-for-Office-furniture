7c02d977ed0bd52ba4205094c8c24699c22d2aa5
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-07 14:47:38 +0000

    Added Sequelize model for BusinessDetails table with associations

--- Backend/models/BusinessDetails.js
+++ Backend/models/BusinessDetails.js
@@ -1,12 +1,23 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/db');
+const Customer = require('./Customer');
 
 const BusinessDetails = sequelize.define('BusinessDetails', {
-    businessName: { type: DataTypes.STRING },
-    registrationNumber: { type: DataTypes.STRING },
+    businessName: { type: DataTypes.STRING, allowNull: false },
+    registrationNumber: { type: DataTypes.STRING, allowNull: false },
     industry: { type: DataTypes.STRING },
     revenue: { type: DataTypes.DECIMAL },
-    customerId: { type: DataTypes.INTEGER, references: { model: 'Customers', key: 'id' } }
+    customerId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Customer,
+            key: 'customerID',
+        },
+    },
 });
+
+BusinessDetails.belongsTo(Customer, { foreignKey: 'customerId', as: 'customer' });
+Customer.hasOne(BusinessDetails, { foreignKey: 'customerId', as: 'businessDetails' });
 
 module.exports = BusinessDetails;
