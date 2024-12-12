7b323d0eba4dff0d5fd59ff31f6afa47c39ff4b2
Harish khumkar <harishkhumkar95@gmail.com>
2024-12-12 12:57:23 +0000

    Aws deployment Database configuration changes

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -5,13 +5,29 @@ const sequelize = new Sequelize('loan_system2', 'root', 'root', {
     host: 'localhost',
     dialect: 'mysql',
     define: {
-        defaultPrimaryKey: false, // Prevent Sequelize from adding the default `id` column
+        defaultPrimaryKey: false, // Prevent  default `id` column
         timestamps: false, // Disable createdAt and updatedAt globally
     },
-    logging: console.log // Enable query logging for debugging
+    logging: console.log 
 });
 
-// Test the database connection
+// for aws deployment databaseb configuration changes Changes
+
+// const sequelize = new Sequelize('loan_system2', 'admin', 'admin123', {
+//     host: 'database-1.cneoesewi0ty.eu-west-1.rds.amazonaws.com',
+//     dialect: 'mysql',
+
+//     define: {
+//         defaultPrimaryKey: false, // Prevent Sequelize from adding the default `id` column
+//         timestamps: false, // Disable createdAt and updatedAt globally
+//     },
+    
+//     dialectOptions: {
+//         connectTimeout: 60000 // Increase timeout to 60 seconds
+//     },
+//     logging: console.log // Enable query logging for debugging
+// });
+
 sequelize.authenticate()
     .then(() => {
         console.log('Connected to MySQL database.');
